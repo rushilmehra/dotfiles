163d5e82325bdad4b391ab51313b71cd7a0558af
Rushil Mehra <rmehra@cloudflare.com>
2024-02-06 00:08:22 -0800

    bruh

--- README.md
+++
@@ -1,5 +0,0 @@
-# Simple Steps
-
-1. Check the Brewfile and install deps (neovim, fish, tmux, etc)
-2. Install Hacker Nerd Font, set font in iterm settings (brew cask)
-
--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 
 # vim keybinds
 fish_hybrid_key_bindings
@@ -14,4 +14,3 @@ alias ls="ls -laG"
 set PATH /home/rmehra/lua-language-server/bin/ $PATH
 set PATH /home/rmehra/.cargo/bin $PATH
 set PATH /home/rmehra/go/bin/ $PATH
-starship init fish | source
